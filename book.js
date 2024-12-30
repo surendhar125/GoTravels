b89cf49c0de9863d36d071e7f1e91976d0c61e5b
= <surendarkarthi4@gmail.com>
2024-12-30 19:47:56 +0530

    book button done

--- book.js
+++ book.js
@@ -1,42 +1,42 @@
 
-
 function personDetailss(){
   const noOfPerson =document.getElementById('noOfPerson').value;
   let personHtml= document.querySelector('.personDetailsGroup');
-    
-  for(let i=1; i<=noOfPerson;i++){
-    
-      personHtml.innerHTML+=`
-      <form class="personDetails">
-      <h1>Person ${i} Details</h1>
-      <hr>
-        <div class="fName">
-          <lable class="label" for="firstName">First Name</lable>
-          <input type="text" id="firstName" required>
-        </div>
-        <div class="lName">
-          <lable class="label" for="firstName">Last Name</lable>
-          <input type="text" id="firstName"  required>
-        </div>
-
-      <label class="label" for="age">Age</label><br>
-      <input type="number" id="age" required><br>
-      <div class="gender">
-        <label class="label" for="gender">Gender</label><br>
-        <div class="genderOption">
-          <div>
-            <label for="male" >Male</label>
-            <input type="radio" id="male" value="Male"  name="gender">
+  const no=noOfPerson;
+  if(noOfPerson >0){  
+    for(let i=1; i<=noOfPerson;i++){
+      
+        personHtml.innerHTML+=`
+        <form class="personDetails">
+        <h1>Person ${i} Details</h1>
+        <hr>
+          <div class="fName">
+            <lable class="label" for="firstName">First Name</lable>
+            <input type="text" id="firstName" required>
           </div>
-          <div>
-            <label for="female">Female</label>
-            <input type="radio" id="female" value="Female" name="gender">
+          <div class="lName">
+            <lable class="label" for="firstName">Last Name</lable>
+            <input type="text" id="firstName"  required>
           </div>
-      </div>
-    </form>`;
 
-  };  
-  if(noOfPerson=>1){
+        <label class="label" for="age">Age</label><br>
+        <input type="number" id="age" required><br>
+        <div class="gender">
+          <label class="label" for="gender">Gender</label><br>
+          <div class="genderOption">
+            <div>
+              <label for="male" >Male</label>
+              <input type="radio" id="male" value="Male"  name="gender">
+            </div>
+            <div>
+              <label for="female">Female</label>
+              <input type="radio" id="female" value="Female" name="gender">
+            </div>
+        </div>
+      </form>`;
+
+    };  
+
     personHtml.innerHTML+=`<a href="payment.html"><button class="formButton personDetailsButton">Proceed To Payment</button></a>`;
   };
 };
--- common files/totopicon.js
+++ common files/totopicon.js
@@ -3,7 +3,7 @@ var totop = document.querySelector(".to-top img");
 window.addEventListener("scroll", ()=>{
   if(window.pageYOffset >300){
     totop.classList.add("active");
-  }   
+  }
   else{
     totop.classList.remove("active");
   }
